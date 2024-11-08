cf7a673a4217a38b269c3b759685f93b4b77d808
brennankapollock <brennankapollock@gmail.com>
2024-11-07 22:51:19 -0800

    init

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,22 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+
+const config = {
+    title: "",
+    description: "Velvet Metal Documentation",
+    // author of site displayed on the bottom of your site
+    author: "brennan k.a. pollock",
+    // logo image
+    authorLogo: "/images/logo.svg",
+    // url to author website
+    domain: "https://brennankapollock.com/",
+    // links to the pages you want to link to in the navbar
+    navLinks: [{ href: "/about", name: "About" }],
+    navbarTitle: {
+        text: "",
+        logo: "/images/black-logo.png", // optional
+      },
+  };
+  
+  export default config;
